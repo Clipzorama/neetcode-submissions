d23f68fc574ca2c02e63319a5d01fec42a4ad17c
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2026-08-11 12:31:37 +0200

    Add: string-encode-and-decode - submission-8

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for word in strs:
+            res += str(len(word)) + "#" + word
+        
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res, i = [], 0
+
+        while i < len(s):
+            j = i
+
+            while s[j] != "#":
+                j += 1
+            
+            # length is the number that is from the encode but coverted to int
+            length = int(s[i:j])
+            # slicing from the first letter in word after # to the end of the word    
+            # (exclusive)
+
+            res.append(s[j + 1: j + 1 + length])
+
+            # Continues to the next word
+            i = j + 1 + length
+        
+        return res

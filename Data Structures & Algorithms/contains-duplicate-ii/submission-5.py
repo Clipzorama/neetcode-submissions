3afe26b366c950cf0dc48eb6a00ee5056c527f7a
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2026-09-09 14:48:57 +0200

    Add: contains-duplicate-ii - submission-5

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        window = set()
+
+        # this one is alot more simpler and we're using a set which comes in handy with the sliding window.
+
+        for right in range(len(nums)):
+            if nums[right] in window:
+                return True
+
+            # if not duplicate we add
+            window.add(nums[right])
+
+            # but we make sure that it satisfies the requirement
+            if len(window) > k:
+                window.remove(nums[right - k])
+
+        return False

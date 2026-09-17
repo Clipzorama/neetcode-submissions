e2a24ebdd3f19e69b9788577c8ba5e1c2c07f7b5
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2026-09-17 23:47:00 +0200

    Update submission-4.py

--- DSA Organized/Easy/Sliding-Window/buy-and-sell-crypto/submission-4.py
+++ DSA Organized/Easy/Sliding-Window/buy-and-sell-crypto/submission-4.py
@@ -1,4 +1,10 @@
-# Two Pointer Solution
+# Two Pointer Solution 
+
+# time complextiy: O(n)
+# space complexity: O(1)
+
+
+# pretty self explanatory. just read the code
 
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
@@ -19,3 +25,6 @@ class Solution:
 
         return maxP
         
+# Input: prices = [10,1,5,6,7,1]
+
+# Output: 6

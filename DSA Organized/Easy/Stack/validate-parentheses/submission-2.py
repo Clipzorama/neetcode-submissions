b78c91227c5c161e5d14c8d64117e493c10b2cfa
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2026-09-18 17:27:08 +0200

    some changes in the notes

--- DSA Organized/Easy/Stack/validate-parentheses/submission-2.py
+++ DSA Organized/Easy/Stack/validate-parentheses/submission-2.py
@@ -12,6 +12,7 @@ class Solution:
 
         for char in s:
 
+            # this if-statement checks if the element in iteration is a closed parenthesis or not 
             if char in pairs:
 
                 # checking if the stack is empty or the last element contains a closing 
--- DSA Organized/Easy/Two-Pointers/merge-sorted-array/submission-1.py
+++ DSA Organized/Easy/Two-Pointers/merge-sorted-array/submission-1.py
@@ -1,5 +1,5 @@
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         # another attempt of doing this solution to understand the pattern 
 
         i = m - 1 # highest number in nums1
